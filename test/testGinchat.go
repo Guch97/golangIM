3f671253e06b1641d3e9b04693ee2fceb1e5a720
xieshuang <930571051@qq.com>
2023-08-02 18:08:18 +0800

    db update

--- test/testGinchat.go
+++ test/testGinchat.go
@@ -15,20 +15,19 @@ func main() {
 	// 迁移 schema
 	db.AutoMigrate(&models.UserBasic{})
 
-	//// Create
-	//db.Create(&models.UserBasic{})
-	//
-	//// Read
-	//var product Product
-	//db.First(&product, 1)                 // 根据整型主键查找
-	//db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
-	//
-	//// Update - 将 product 的 price 更新为 200
-	//db.Model(&product).Update("Price", 200)
-	//// Update - 更新多个字段
+	// Create
+	user := &models.UserBasic{}
+	user.Name = "神专"
+	db.Create(user)
+
+	// Read
+	db.First(user, 1) // 根据整型主键查找
+
+	// Update - 将 product 的 price 更新为 200
+	db.Model(user).Update("PassWord", "1234")
+	// Update - 更新多个字段
 	//db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
 	//db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-	//
-	//// Delete - 删除 product
+	// Delete - 删除 product
 	//db.Delete(&product, 1)
 }
